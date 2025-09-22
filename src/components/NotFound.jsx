2ba51231bef27977f21e37d923fd23fabdd197a9
udoka ubah <udokaubah9@gmail.com>
2025-09-22 14:33:34 -0700

    fix: updated page not found

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,3 +1,14 @@
 export default function NotFound() {
-  return <p>Page Not Found</p>;
+  return (
+    <div className="relative">
+      <BackButton />
+      <h1 className="font-bold text-2xl text-center py-3 px-4 tracking-wide">
+        Page Not Found!
+      </h1>
+
+      <p className="flex items-center justify-center h-[50vh] text-3xl">404</p>
+
+      <Footer type="home" />
+    </div>
+  );
 }
